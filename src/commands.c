356cd387e2193a0613ddce08b5d08989afdb502c
MinwooPark <alsdndia789@naver.com>
2017-10-09 00:01:39 +0900

    mysh-0

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,47 @@
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
 
+	char Buffer[300] = {0, };
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
   // TODO: Fill it!
-  return 0;
+  else {
+	chdir(argv[1]);
+    return 0;
+  }
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
   // TODO: Fill it!
-
-  return 0;
+  else {
+    getcwd(Buffer , 300);
+    return 0;
+    }
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
+  if((strcmp(argv[0],"cd") ==0) && (argc == 2)) { //argv's first word is cd , 2 word
+    if(strcmp(argv[1],"cd") == 0) {
+	return 0;
+    }
   return 1;
+  }
+  else { 
+  return 0;
+  }
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
+  if(strcmp(argv[0], "pwd") ==0 && (argc == 1)) {
   return 1;
+  }
+  else {
+  return 0;
+  }
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,31 @@
 #include "utils.h"
+#include <string.h>
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+
+	int i = 0;
+	*argv = (char**) malloc (sizeof(char)*100);
+	**argv = (char*) malloc (sizeof(char)*100);
+
+	char* input= (char*) malloc (sizeof(char)*100);
+	strcpy(input, command);
+	
+	char blank[] = " \n\t";
+
+	char* ptr;
+	ptr = strtok(input,blank);
+	
+	*argc = 0;
+
+	 while (ptr != NULL) {
+	   (*argv)[i] = (char*)malloc (sizeof(char)*100);
+	   strcpy((*argv)[i++], ptr);
+	   ptr = strtok(NULL,blank);
+	   (*argc) = (*argc) +1; //count argc 
+	  }	
+
 }
